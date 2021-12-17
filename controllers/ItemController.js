858268231335563366d544c5f94635f41cb0586d
MK202019 <91296207+MK202019@users.noreply.github.com>
2021-12-17 09:10:25 +0900

    aaa

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -4,5 +4,15 @@ exports.show = (req, res) => {
     const id = req.params.id
     console.log(id)
     let data = {}
-    data.item = item
+    data.item = item.find(id)
+    res.render('item/show,ejs',data)
+}
+
+exports.show = (res,req) => {
+    const id = req.params.id
+    console.log(id)
+    let data = {}
+    data.item = item.find(id)
+    
+
 }
